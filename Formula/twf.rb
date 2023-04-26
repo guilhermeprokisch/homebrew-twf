f6eda14e5fcb8d162d54959b692cc86396439d05
Guilherme Prokisch <guilherme.prokisch@gmail.com>
2023-04-26 12:57:23 +0200

    chore: changed repo name

--- Formula/twf.rb
+++ Formula/twf.rb
@@ -1,17 +1,17 @@
 class Twf < Formula
   desc "Standalone tree view file explorer, inspired by fzf"
-  homepage "https://github.com/wvanlint/twf"
-  head "https://github.com/wvanlint/twf.git"
+  homepage "https://github.com/guilhermeprokisch/twf"
+  head "https://github.com/guilhermeprokisch/twf.git"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", "-o", bin/"twf", "github.com/wvanlint/twf/cmd/twf"
+    system "go", "build", "-o", bin/"twf", "github.com/guilhermeprokisch/twf/cmd/twf"
   end
 
   def caveats
     <<~EOS
-      See https://github.com/wvanlint/twf for application-specific integrations,
+      See https://github.com/guilhermeprokisch/twf for application-specific integrations,
       such as with vim and zsh.
     EOS
   end
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Homebrew twf tap
 
 ## How do I install these formulae?
-`brew install wvanlint/twf/<formula>`
+`brew install guilhermeprokisch/twf/<formula>`
 
-Or `brew tap wvanlint/twf` and then `brew install <formula>`.
+Or `brew tap guilhermeprokisch/twf` and then `brew install <formula>`.
 
 ## Documentation
 `brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
